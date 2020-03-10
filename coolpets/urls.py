3e71d48a54fc65e5926694da10117e1e99b9c4d2
Fred Dorn <freddorn1@gmail.com>
2020-03-10 22:49:15 +0000

    Add urls to coolpets/urls.py

--- coolpets/urls.py
+++ coolpets/urls.py
@@ -14,8 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from pages.views import home_view
 
 urlpatterns = [
+
+    path('', home_view, name="home"),
+    path('pages/', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
+    path('search/', include('search.urls')),
+
     path('admin/', admin.site.urls),
 ]
+
+# only add this in when in debug mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
