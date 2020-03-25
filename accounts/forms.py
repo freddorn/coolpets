1388ff10fec9d36b9859da786a2a909806d0227b
Fred Dorn <freddorn1@gmail.com>
2020-03-25 23:09:12 +0000

    Add form data to accounts/forms.py

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    """ Form to register a new user to User table """
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """ Form to update User info username and email """
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
