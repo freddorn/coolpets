9667ce3fb51cb6045d08b2799d8bea6bdedb2048
Fred Dorn <freddorn1@gmail.com>
2020-04-08 23:44:38 +0000

    Add models.py to the products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    """ Model for product instances """
+
+    LEASHES = 'Leashes'
+    COLLARS = 'Collars'
+    BEDS = 'Beds'
+    CARRIERS = 'Carriers'
+    FEEDING = 'Feeding'
+    HARNESSES = 'Harnesses'
+    TOYS = 'Toys'
+    CATEGORY_CHOICES = [
+        (LEASHES, 'Leashes'),
+        (COLLARS, 'Collars'),
+        (BEDS, 'Beds'),
+        (CARRIERS, 'Carriers'),
+        (FEEDING, 'Feeding'),
+        (HARNESSES, 'Harnesses'),
+        (TOYS, 'Toys'),
+    ]
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default=LEASHES,
+    )
+    title = models.CharField(max_length=100)
+    product_image1 = models.ImageField(upload_to="product_images", blank=True)
+    product_image2 = models.ImageField(
+        upload_to="product_images", blank=True, null=True)
+    product_image3 = models.ImageField(
+        upload_to="product_images", blank=True, null=True)
+    product_image4 = models.ImageField(
+        upload_to="product_images", blank=True, null=True)
+    product_image5 = models.ImageField(
+        upload_to="product_images", blank=True, null=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    tags = models.CharField(max_length=300)
+    num_in_stock = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(100)])
+    featured = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        """ Gets absolute url including listing id as pk for dynamic url """
+        return reverse('listing-detail', kwargs={'pk': self.id})
+
+    def __str__(self):
+        return self.title
