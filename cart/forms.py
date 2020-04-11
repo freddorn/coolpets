cba0cb355ddd875caf4f7e4ea159a0820d223e3b
Fred Dorn <freddorn1@gmail.com>
2020-04-11 01:17:19 +0000

    Add forms.py to cart app

---
+++ cart/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Order, OrderItem
+
+
+class OrderItemForm(forms.ModelForm):
+    """ form for cart view, so the user can update quantity of product they want to order. """
+    quantity = forms.IntegerField(
+        min_value=0,
+        max_value=10
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = [
+            'quantity'
+        ]
+
+
+class NewOrderForm(forms.ModelForm):
+
+    """ Form to collect shipping information for order on checkout_info page """
+    class Meta:
+        model = Order
+        fields = [
+            'full_name',
+            'address_line_1',
+            'address_line_2',
+            'town_or_city',
+            'state',
+            'postcode',
+            'country',
+        ]
