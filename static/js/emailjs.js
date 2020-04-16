d56b547e88cd1a3e1f3993f57fdec6ac8b63a94b
Fred Dorn <freddorn1@gmail.com>
2020-04-16 02:40:26 +0000

    Redesign Contact Page

--- static/js/emailjs.js
+++ static/js/emailjs.js
@@ -1,57 +1,52 @@
-$(document).ready(function () {
-    const contactForm = document.querySelector('#contact-form');
+let sendMail = function (form) {
 
-    contactForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const data = {
-            service_id: "gmail",
-            template_id: "coolpets",
-            user_id: ejs,
-            template_params: {
-                "name": contactForm.name.value,
-                "email": contactForm.email.value,
-                "message": contactForm.message.value,
-            }
-        };
 
-        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-            type: 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        }).done(function () {
-            contact_form_success(contactForm.name.value);
-            $('input').val('');
-            $('textarea').val('');
+    emailjs.send("gmail", "coolpets", {
 
-        }).fail(function (error) {
-            contact_form_error();
-            console.log('Oops... ' + JSON.stringify(error));
-        });
-    });
+        "from_name": form.name.value,
 
-    function contact_form_success(name) {
-        let message = `Thank you ${name}.`;
-        Swal.fire({
-            type: 'success',
-            title: message,
-            text: "Your message has been sent successfully!",
-            confirmButtonText: 'Ok',
-            confirmButtonColor: '#47b7f8'
-        }).then((result) => {
-            if (result.value) {
-                window.location.replace("/");
-            }
-        });
-    }
+        "from_email": form.emailaddress.value,
 
-    function contact_form_error() {
-        let message = `Oops!`;
-        Swal.fire({
-            type: 'warning',
-            title: message,
-            text: "Something went wrong with our contact form, please try again",
-            showConfirmButton: false,
-            timer: 2000,
-        })
-    }
-})
+        "message": form.enquiry.value
+
+    })
+
+        .then(
+
+            function (response) {
+
+                console.log("SUCCES", response);
+
+                alert("Your message has been sent successfully");
+
+                document.getElementById('form').reset();
+
+            },
+
+            function (error) {
+
+                console.log("FAILED", error);
+
+                alert("Message was not sent");
+
+                document.getElementById('form').reset();
+
+            });
+
+    return false;
+
+};
+
+
+
+
+
+async function init() {
+
+    await sendMail();
+
+}
+
+
+
+init();
