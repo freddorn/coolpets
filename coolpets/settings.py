ac781e63b02530ab3ed746d06e67177353edbc0f
Fred Dorn <freddorn1@gmail.com>
2020-05-19 03:22:26 +0000

    Troubleshoot AWS problem

--- coolpets/settings.py
+++ coolpets/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = '/media/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
