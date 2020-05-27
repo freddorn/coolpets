3987a98d8ad88f94ff75c33d9fcda42f032bf43e
Fred Dorn <freddorn1@gmail.com>
2020-05-27 07:23:43 +0000

    Test stripe problem

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,12 +1,32 @@
-var stripe = Stripe('pk_test_Bg9povZBdi6gNH7dcYIoVmAm00lXpzmA8k');
+$(function () {
+    $("#payment-form").submit(function () {
+        var form = this;
+        var card = {
+            number: $("#id_credit_card_number").val(),
+            expMonth: $("#id_expiry_month").val(),
+            expYear: $("#id_expiry_year").val(),
+            cvc: $("#id_cvv").val()
+        };
 
-stripe.redirectToCheckout({
-    // Make the id field from the Checkout Session creation API response
-    // available to this file, so you can provide it as parameter here
-    // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-    sessionId: '{{CHECKOUT_SESSION_ID}}'
-}).then(function (result) {
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `result.error.message`.
+        Stripe.createToken(card, function (status, response) {
+            if (status === 200) {
+                $("#credit-card-errors").hide();
+                $("#id_stripe_id").val(response.id);
+
+                // Prevent the credit card details from being submitted
+                // to our server
+                $("#id_credit_card_number").removeAttr('name');
+                $("#id_cvv").removeAttr('name');
+                $("#id_expiry_month").removeAttr('name');
+                $("#id_expiry_year").removeAttr('name');
+
+                form.submit();
+            } else {
+                $("#stripe-error-message").text(response.error.message);
+                $("#credit-card-errors").show();
+                $("#validate_card_btn").attr("disabled", false);
+            }
+        });
+        return false;
+    });
 });
