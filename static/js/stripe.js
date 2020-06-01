e7debcd11bb2a272d0b64f8cd796b120ddd83b2e
Fred Dorn <freddorn1@gmail.com>
2020-06-01 00:13:21 +0000

    Troubleshoot stripe

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,23 +1,10 @@
-
-
-// // All code below provided by https://stripe.com/docs/payments/cards/collecting/web
-
-let stripe = Stripe(s_pub);
-let elements = stripe.elements();
-
-$('#submit-payment-btn').click(function() {
-    startCheckout();
+stripe.redirectToCheckout({
+    // Make the id field from the Checkout Session creation API response
+    // available to this file, so you can provide it as parameter here
+    // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+    sessionId: '{{s_id}}'
+}).then(function (result) {
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `result.error.message`.
 });
-
-/**
- * Activates stripe v3 checkout page
- */
-async function startCheckout() {
-    const {error} = await stripe.redirectToCheckout({
-        sessionId: s_id
-    });
-
-    if (error) {
-        alert('Something went wrong with the payment, please try again.');
-    }
-}
