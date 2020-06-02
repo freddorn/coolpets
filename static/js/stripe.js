78d2cf0ff5c72ad27c4034c6c3d40744ca20ad9d
Fred Dorn <freddorn1@gmail.com>
2020-06-02 20:35:28 +0000

    Test stripe

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -17,7 +17,7 @@ stripe.redirectToCheckout({
   // Make the id field from the Checkout Session creation API response
   // available to this file, so you can provide it as parameter here
   // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
-  sessionId: 's_id'
+  sessionId: 'pi_1GpgkxCe2EMmKgcFrZLE1mxA'
 }).then(function (result) {
   // If `redirectToCheckout` fails due to a browser or network
   // error, display the localized error message to your customer
