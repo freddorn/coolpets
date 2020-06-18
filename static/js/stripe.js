7fe0b4d45688af5c290182f35658109824707b8b
Fred Dorn <freddorn1@gmail.com>
2020-06-18 02:45:46 +0000

    Edit stripe.js

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,29 +1,25 @@
 console.log("Sanity check!");
 
-// Get Stripe publishable key
-fetch("/config/")
-    .then((result) => {
-        return result.json();
-    })
-    .then((data) => {
-        // Initialize Stripe.js
-        const stripe = Stripe(data.publicKey);
+const s_pub = ('STRIPE_PUBLISHABLE');
 
-        // new
-        // Event handler
-        document.querySelector("#submitBtn").addEventListener("click", () => {
-            // Get Checkout Session ID
-            fetch("/create-checkout-session/")
-                .then((result) => {
-                    return result.json();
-                })
-                .then((data) => {
-                    console.log(data);
-                    // Redirect to Stripe Checkout
-                    return stripe.redirectToCheckout({ sessionId: data.sessionId });
-                })
-                .then((res) => {
-                    console.log(res);
-                });
-        });
+
+
+let stripe = Stripe(s_pub);
+let elements = stripe.elements();
+
+$('#submit-payment-btn').click(function () {
+    startCheckout();
+});
+
+/**
+ * Activates stripe v3 checkout page
+ */
+async function startCheckout() {
+    const { error } = await stripe.redirectToCheckout({
+        sessionId: s_id
     });
+
+    if (error) {
+        alert('Something went wrong with your payment, please try again.');
+    }
+}
